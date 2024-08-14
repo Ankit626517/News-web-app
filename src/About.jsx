e508c29f233d1a4521d0be9972c07a7b58893ae9
Ankit Rathor <114902967+Ankit626517@users.noreply.github.com>
2024-08-14 11:04:42 +0530

    Second commit

--- src/About.jsx
+++ src/About.jsx
@@ -1,16 +1,169 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+
+
 export default class About extends Component {
   render() {
     return (
-      <div className='about'>
-        <div>
-          <img src='' />
-          <h1>Name</h1>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit minus nostrum doloremque ipsa sint maiores nulla officiis cumque iste necessitatibus?</p>
+      <div>
+        <div className="about_section_main">
+          {/* <img src="./WhatsApp.jpeg" alt="" style={{ width: "100%", height: "400rem" }} /> */}
+          <div className="about_card_main">
+            <img
+              src="../public/AnjanaOmKashyap.jpg"
+              alt=" note found"
+              style={{ width: "100%", height: "100rem", margin: "3%" }}
+            />
+
+            <h2>
+              {" "}
+              <b>Anjana Om Kashyap</b>
+            </h2>
+
+            <p>
+              Anjana Om Kashyap is an Indian news anchor working with Hindi news
+              channel Aaj Tak as a Managing Editor.[1] Kashyap started her
+              journalistic career by joining the public broadcaster Doordarshan
+              before moving to Zee News. She then went to News 24 before
+              settling into Aaj Tak.[2]
+            </p>
+            <a href="https://en.wikipedia.org/wiki/Anjana_Om_Kashyap">
+              Readmore
+            </a>
+          </div>
+          <div className="about_card_main">
+            <img
+              src="./swetaji.jpeg"
+              alt="img note found"
+              style={{ width: "100%", height: "100rem", margin: "3%" }}
+            />
+
+            <h2>
+              {" "}
+              <b>Sweta Singh</b>
+            </h2>
+
+            <p>
+            Singh started her career while still in first year of graduate studies in Patna University. She has several bylines to her name in The Times of India Patna edition and Hindustan Times Patna edition, before she switched to electronic media in 1998. She worked for Zee News and Sahara, before joining Aaj Tak in 2002.[3] She is known for her expertise in covering sports-related news. Her show Sourav ka Sixer won the award for best sports programme by Sports Journalism Federation of India (SJFI) in 2005.[4] 
+            </p>
+            <a href="https://en.wikipedia.org/wiki/Sweta_Singh">
+              Readmore
+            </a>
+          </div>
+          <div className="about_card_main">
+            <img
+              src="./Razatsir.jpeg"
+              alt="img note found"
+              style={{ width: "100%", height: "100rem", margin: "3%" }}
+            />
+
+            <h2>
+              {" "}
+              <b>Rajat Sharma</b>
+            </h2>
+
+            <p>
+              Rajat Sharma (born 18 February 1957) is an Indian journalist and
+              businessperson who is the chairman and Editor-in-chief of India
+              TV, an Indian news outlet.[1][2] Sharma is known for his show Aap
+              Ki Adalat in which he is seen interviewing politicians and
+              celebrities in a courtroom-like setting.
+            </p>
+            <a href="https://en.wikipedia.org/wiki/Rajat_Sharma">Readmore</a>
+          </div>
+          <div className="about_card_main">
+            <img
+              src="./AnjanaOmKashyap.jpg"
+              alt="img note found"
+              style={{ width: "100%", height: "100rem", margin: "3%" }}
+            />
+
+            <h2>
+              {" "}
+              <b>Anjana Om Kashyap</b>
+            </h2>
+
+            <p>
+              Anjana Om Kashyap is an Indian news anchor working with Hindi news
+              channel Aaj Tak as a Managing Editor.[1] Kashyap started her
+              journalistic career by joining the public broadcaster Doordarshan
+              before moving to Zee News. She then went to News 24 before
+              settling into Aaj Tak.[2]
+            </p>
+            <a href="https://en.wikipedia.org/wiki/Anjana_Om_Kashyap">
+              Readmore
+            </a>
+          </div>
+          <div className="about_card_main">
+            <img
+              src="./AnjanaOmKashyap.jpg"
+              alt="img note found"
+              style={{ width: "100%", height: "100rem", margin: "3%" }}
+            />
+
+            <h2>
+              {" "}
+              <b>Sweta Singh</b>
+            </h2>
+
+            <p>
+              Anjana Om Kashyap is an Indian news anchor working with Hindi news
+              channel Aaj Tak as a Managing Editor.[1] Kashyap started her
+              journalistic career by joining the public broadcaster Doordarshan
+              before moving to Zee News. She then went to News 24 before
+              settling into Aaj Tak.[2]
+            </p>
+            <a href="https://en.wikipedia.org/wiki/Anjana_Om_Kashyap">
+              Readmore
+            </a>
+          </div>
+          <div className="about_card_main">
+            <img
+              src="./AnjanaOmKashyap.jpg"
+              alt="img note found"
+              style={{ width: "100%", height: "100rem", margin: "3%" }}
+            />
+
+            <h2>
+              {" "}
+              <b>Anjana Om Kashyap</b>
+            </h2>
+
+            <p>
+              Anjana Om Kashyap is an Indian news anchor working with Hindi news
+              channel Aaj Tak as a Managing Editor.[1] Kashyap started her
+              journalistic career by joining the public broadcaster Doordarshan
+              before moving to Zee News. She then went to News 24 before
+              settling into Aaj Tak.[2]
+            </p>
+            <a href="https://en.wikipedia.org/wiki/Anjana_Om_Kashyap">
+              Readmore
+            </a>
+          </div>
+          <div className="about_card_main">
+            <img
+              src='./AnjanaOmKashyap.jpg'
+              alt="img note found"
+              style={{ width: "100%", height: "100rem", margin: "3%" }}
+            />
+
+            <h2>
+              {" "}
+              <b>Anjana Om Kashyap</b>
+            </h2>
+
+            <p>
+              Anjana Om Kashyap is an Indian news anchor working with Hindi news
+              channel Aaj Tak as a Managing Editor.[1] Kashyap started her
+              journalistic career by joining the public broadcaster Doordarshan
+              before moving to Zee News. She then went to News 24 before
+              settling into Aaj Tak.[2]
+            </p>
+            <a href="https://en.wikipedia.org/wiki/Anjana_Om_Kashyap">
+              Readmore
+            </a>
+          </div>
         </div>
-
       </div>
-
-    )
+    );
   }
 }
